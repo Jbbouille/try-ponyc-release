b211f0eb8d093baf7c1cbe74a90201f0d52f7b5b
Jean-Baptiste Petit <jbpetit@excilys.com>
2016-04-30 15:46:13 +0200

    Pony_version

--- tools/build.sh
+++ tools/build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-echo 'before'
-echo $TRAVIS_BRANCH
 if [[ $TRAVIS_BRANCH =~ "release/" ]]; then
-	echo 'IN'
+	echo $PONY_VERSION
 	docker build -t ponyc-arch /home/travis/build/Jbbouille/try-ponyc-release/tools/archlinux/
 	docker build -t ponyc-ubuntu /home/travis/build/Jbbouille/try-ponyc-release/tools/ubuntu/
 	docker build -t ponyc-red-hat /home/travis/build/Jbbouille/try-ponyc-release/tools/red-hat/
@@ -13,4 +11,3 @@ if [[ $TRAVIS_BRANCH =~ "release/" ]]; then
 	docker run ponyc-red-hat
 	exit 0
 fi
-echo 'after'
