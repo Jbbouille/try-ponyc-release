83ccf604af352d02b6a6a3c336cc5dcf6ed66bca
Jean-Baptiste Petit <jbpetit@excilys.com>
2016-04-30 18:32:40 +0200

    using ARGœ

--- tools/build.sh
+++ tools/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 if [[ $TRAVIS_BRANCH =~ "release/" ]]; then
-	docker build -t ponyc-arch /home/travis/build/Jbbouille/try-ponyc-release/tools/archlinux/
-	docker build -t ponyc-ubuntu /home/travis/build/Jbbouille/try-ponyc-release/tools/ubuntu/
-	docker build -t ponyc-red-hat /home/travis/build/Jbbouille/try-ponyc-release/tools/red-hat/
+	docker build --build-arg TRAVIS_BRANCH=$TRAVIS_BRANCH -t ponyc-arch /home/travis/build/Jbbouille/try-ponyc-release/tools/archlinux/
+	docker build --build-arg TRAVIS_BRANCH=$TRAVIS_BRANCH -t ponyc-ubuntu /home/travis/build/Jbbouille/try-ponyc-release/tools/ubuntu/
+	docker build --build-arg TRAVIS_BRANCH=$TRAVIS_BRANCH -t ponyc-red-hat /home/travis/build/Jbbouille/try-ponyc-release/tools/red-hat/
 
-	docker run ponyc-arch
-	docker run ponyc-ubuntu
-	docker run ponyc-red-hat
+	docker run -e TRAVIS_BRANCH=$TRAVIS_BRANCH GITHUB_TOKEN=$GITHUB_TOKEN PONY_VERSION=$PONY_VERSION ponyc-arch
+	docker run -e TRAVIS_BRANCH=$TRAVIS_BRANCH GITHUB_TOKEN=$GITHUB_TOKEN PONY_VERSION=$PONY_VERSION ponyc-ubuntu
+	docker run -e TRAVIS_BRANCH=$TRAVIS_BRANCH GITHUB_TOKEN=$GITHUB_TOKEN PONY_VERSION=$PONY_VERSION ponyc-red-hat
 	exit 0
 fi
