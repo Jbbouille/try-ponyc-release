448ca773f21553f7da51452dd4f8099b8a2e74ce
Jean-Baptiste Petit <jbpetit@excilys.com>
2016-04-30 15:12:47 +0200

    travis_branch

--- tools/build.sh
+++ tools/build.sh
@@ -2,7 +2,7 @@
 
 echo 'before'
 if [ $TRAVIS_BRANCH == 'release/'* ]; then
-	echo 'IN release'
+	echo 'IN'
 	docker build -t ponyc-arch /home/travis/build/Jbbouille/try-ponyc-release/tools/archlinux/
 	docker build -t ponyc-ubuntu /home/travis/build/Jbbouille/try-ponyc-release/tools/ubuntu/
 	docker build -t ponyc-red-hat /home/travis/build/Jbbouille/try-ponyc-release/tools/red-hat/
