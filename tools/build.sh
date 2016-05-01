01873126009467195369e3bb8a9b44578ca170b6
Jean-Baptiste Petit <jbpetit@excilys.com>
2016-05-01 16:49:22 +0200

    not local images

--- tools/build.sh
+++ tools/build.sh
@@ -5,8 +5,8 @@ if [[ $TRAVIS_BRANCH =~ "release/" ]]; then
 	docker pull jbbouille/ponyc-ubuntu
 	docker pull jbbouille/ponyc-fedora
 
-	docker run -e TRAVIS_BRANCH=$TRAVIS_BRANCH -e GITHUB_TOKEN=$GITHUB_TOKEN -e PONY_VERSION=$PONY_VERSION ponyc-arch
-	docker run -e TRAVIS_BRANCH=$TRAVIS_BRANCH -e GITHUB_TOKEN=$GITHUB_TOKEN -e PONY_VERSION=$PONY_VERSION ponyc-ubuntu
-	docker run -e TRAVIS_BRANCH=$TRAVIS_BRANCH -e GITHUB_TOKEN=$GITHUB_TOKEN -e PONY_VERSION=$PONY_VERSION ponyc-fedora
+	docker run -e TRAVIS_BRANCH=$TRAVIS_BRANCH -e GITHUB_TOKEN=$GITHUB_TOKEN -e PONY_VERSION=$PONY_VERSION jbbouille/ponyc-arch
+	docker run -e TRAVIS_BRANCH=$TRAVIS_BRANCH -e GITHUB_TOKEN=$GITHUB_TOKEN -e PONY_VERSION=$PONY_VERSION jbbouille/ponyc-ubuntu
+	docker run -e TRAVIS_BRANCH=$TRAVIS_BRANCH -e GITHUB_TOKEN=$GITHUB_TOKEN -e PONY_VERSION=$PONY_VERSION jbbouille/ponyc-fedora
 	exit 0
 fi
